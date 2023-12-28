a10b2201c19ded688e604f6e5fac08c88cd1d4bf
Scott <fffff40103@gmail.com>
2023-12-28 13:34:01 +0800

    java config adjust

--- crypto4/src/main/java/spring/mvc/crypto/config/WebSocketConfig.java
+++ crypto4/src/main/java/spring/mvc/crypto/config/WebSocketConfig.java
@@ -5,12 +5,13 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-@Configuration
-@EnableWebSocket
+@Configuration//一個spring的配置項
+@EnableWebSocket//標註讓websocket支持
 public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    	//這是websocket的端點，可以與websocket建立連線
         registry.addHandler(new MyWebSocketHandler(), "/websocket")
                 .setAllowedOrigins("*"); // 允許所有源進行連線，可以根據實際情況修改
 
